bfcb6f1b7d19fa0297cd38db2ae8a24b87e53d16
TOKATL1 <120321214+TOKATL1@users.noreply.github.com>
2022-12-17 09:53:45 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String ogrenci1 = "tarik";
+        String ogrenci2 = "emir";
+        String ogrenci3 = "gulfem";
+
+
+        System.out.println(ogrenci1);
+        System.out.println(ogrenci2);
+        System.out.println(ogrenci3);
+
+        String[] ogrenciler = new String[
+                3];
+
+
+        for (int i = 0; i < ogrenciler.length; i++) {
+
+        System.out.println(ogrenciler[i]);
+         }
+
+
+
+    }
+}
